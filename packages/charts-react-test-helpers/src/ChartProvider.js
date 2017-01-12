5a4efcf49bc64cc32a0b7b5ac30a12c9758b97d1
Josh Black <joshblack@us.ibm.com>
2017-01-12 12:59:57 -0600

    feat: Add test helper package for providing context

---
+++ packages/charts-react-test-helpers/src/ChartProvider.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+/**
+ * It's helpful to leverage component in test scenarios where you're relying on
+ * Jest's `snapshot` feature. Since our components rely on the chart dimensions
+ * being passed in through context, use this component to provide that context
+ * without adding extra nodes to the snapshot output.
+ */
+export default class ChartProvider extends React.PureComponent {
+  static childContextTypes = {
+    chart: React.PropTypes.object,
+  }
+
+  getChildContext() {
+    return {
+      chart: {
+        width: 500,
+        height: 500,
+        margin: {
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        },
+      },
+    };
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
