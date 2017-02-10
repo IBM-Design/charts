179346e23dbd02f0ce5ef3f54463afd806b516f2
Jamie Skinner <jamie@ninthmind.com>
2017-02-10 10:01:05 -0600

    added label wrapping

--- packages/charts-vanilla-radar/src/scripts/index.js
+++ packages/charts-vanilla-radar/src/scripts/index.js
@@ -161,9 +161,11 @@ class Radar {
               'font-size': '0.8em',
               'fill': 'grey',
               'opacity': 0.4,
-              'text-anchor': "middle"
+              'text-anchor': 'middle',
             })
-            .html((d) => { return this.wrap(d.axis)})
+            .text((d)=>{return d.axis})
+            .call(this.wrap, this.cfg.wrapWidth)
+
 
 
 
@@ -185,8 +187,33 @@ class Radar {
     return data
   } // formatData
 
-  wrap(text) {
-    return text.split(' ').join('\n')
+
+  //Taken from http://bl.ocks.org/mbostock/7555321
+  //Wraps SVG text
+  wrap(text, width) {
+    text.each(function() {
+        var text = d3.select(this),
+          words = text.text().split(/\s+/).reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 1.4, // ems
+          y = text.attr("y"),
+          x = text.attr("x"),
+          dy = parseFloat(text.attr("dy")) || 0,
+          tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+
+        while (word = words.pop()) {
+          line.push(word);
+          tspan.text(line.join(" "));
+          if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+          }
+        }
+        });
   }//wrap
 }
 
@@ -202,6 +229,7 @@ function test(data){
     opacityArea: 0.4,
     labelFactor: 50,
     shape: 'circle',
+    wrapWidth: 60
   })
   a.draw()
 }
