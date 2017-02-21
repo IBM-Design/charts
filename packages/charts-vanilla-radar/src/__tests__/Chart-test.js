fb70542ada6dd0c40c910c87744297ead7c07e66
Jamie Skinner <jamie@ninthmind.com>
2017-02-21 15:47:47 -0600

    removing linting errors

--- packages/charts-vanilla-radar/src/__tests__/Chart-test.js
+++ packages/charts-vanilla-radar/src/__tests__/Chart-test.js
@@ -1,7 +1,7 @@
-import Chart from '../';
+// import Chart from '../';
 
 describe('Radar Component', () => {
   it('should render', () => {
-    expect(true).toBe(true, 'This is a placeholder true')
+    expect(true).toBe(true, 'This is a placeholder true');
   });
 });
