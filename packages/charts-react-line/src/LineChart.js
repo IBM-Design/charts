2caefe3a2b9e3b1f140f045db6830c99f3ea1107
Matthew Gallo <mdgallo@us.ibm.com>
2017-10-10 15:02:14 -0500

    Added prop-types package instead of using propTypes via React, this is deprecated in React v16.

--- packages/charts-react-line/src/LineChart.js
+++ packages/charts-react-line/src/LineChart.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import * as shape from 'd3-shape';
 import Chart from '@ibm-design/charts-react-chart';
 import { LeftAxis, BottomAxis } from '@ibm-design/charts-react-axis';
